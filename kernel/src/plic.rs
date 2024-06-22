5ddbe21b01202da90137a836f9a603cc6d64a49a
zhangxuhai <lenshood.zxh@gmail.com>
2024-06-22 15:29:38 +0800

    refactor: use read/write method to op raw address rather than * symbal

--- kernel/src/plic.rs
+++ kernel/src/plic.rs
@@ -9,10 +9,10 @@ use crate::{proc, PLIC_SCLAIM, PLIC_SENABLE, PLIC_SPRIORITY};
 pub(crate) fn plicinit() {
     unsafe {
         // set desired IRQ priorities non-zero (otherwise disabled).
-        let uart_irq_ref = ((PLIC + UART0_IRQ * 4) as *mut u32).as_mut().unwrap();
-        *(uart_irq_ref) = 1;
-        let virtio_irq_ref = ((PLIC + VIRTIO0_IRQ * 4) as *mut u32).as_mut().unwrap();
-        *(virtio_irq_ref) = 1;
+        let uart_irq_ref = (PLIC + UART0_IRQ * 4) as *mut u32;
+        uart_irq_ref.write_volatile(1);
+        let virtio_irq_ref = ((PLIC + VIRTIO0_IRQ * 4) as *mut u32);
+        virtio_irq_ref.write_volatile(1);
     }
 }
 
@@ -22,26 +22,26 @@ pub(crate) fn plicinithart() {
     unsafe {
         // set enable bits for this hart's S-mode
         // for the uart and virtio disk.
-        let senable_ref = (PLIC_SENABLE!(hart) as *mut u32).as_mut().unwrap();
-        *(senable_ref) = (1 << UART0_IRQ) | (1 << VIRTIO0_IRQ);
+        let senable_ref = PLIC_SENABLE!(hart) as *mut u32;
+        senable_ref.write_volatile((1 << UART0_IRQ) | (1 << VIRTIO0_IRQ));
 
         // set this hart's S-mode priority threshold to 0.
-        let spriority_ref = (PLIC_SPRIORITY!(hart) as *mut u32).as_mut().unwrap();
-        *(spriority_ref) = 0;
+        let spriority_ref = PLIC_SPRIORITY!(hart) as *mut u32;
+        spriority_ref.write_volatile(0);
     }
 }
 
 // ask the PLIC what interrupt we should serve.
-pub(crate) fn plic_claim() -> i32 {
+pub(crate) fn plic_claim() -> u32 {
     let hart = cpuid();
-    let irq = PLIC_SCLAIM!(hart) as *const i32;
-    unsafe { *irq }
+    let irq = PLIC_SCLAIM!(hart) as *const u32;
+    unsafe { irq.read_volatile() }
 }
 
 // tell the PLIC we've served this IRQ.
-pub(crate) fn plic_complete(irq: i32) {
+pub(crate) fn plic_complete(irq: u32) {
     let hart = cpuid();
     unsafe {
-        *(PLIC_SCLAIM!(hart) as *mut i32) = irq;
+        (PLIC_SCLAIM!(hart) as *mut u32).write_volatile(irq);
     }
 }
