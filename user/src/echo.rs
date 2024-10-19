c8c0377dbef0481d18e429205067c2661bb87b5a
zhangxuhai <lenshood.zxh@gmail.com>
2024-10-19 15:43:02 +0800

    fix: not call exit in echo

--- user/src/echo.rs
+++ user/src/echo.rs
@@ -1,25 +1,23 @@
 #![no_std]
 #![feature(start)]
 
-use core::mem;
-
-use ulib::stubs::write;
+use kernel::string::strlen;
+use ulib::stubs::{exit, write};
 
 #[start]
 fn main(argc: isize, argv: *const *const u8) -> isize {
     unsafe {
-        let args = core::slice::from_raw_parts(argv, argc as usize);
-
         for i in 1..argc {
-            let sz = mem::size_of_val(args[i as usize].as_ref().unwrap());
-            write(1, args[i as usize], sz as i32);
+            let curr_argv = argv.add(i as usize).read_volatile();
+            let sz = strlen(curr_argv);
+            write(1, curr_argv, sz as i32);
             if i + 1 < argc {
                 write(1, &(' ' as u8) as *const u8, 1);
             } else {
                 write(1, &('\n' as u8) as *const u8, 1);
             }
         }
-    }
 
-    0
+        exit(0);
+    }
 }
